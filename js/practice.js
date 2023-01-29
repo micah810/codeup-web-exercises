6e5ecf85050474d54046d21df227ab604862d82e
Micah Larson <micahlarson810@gmail.com>
2023-01-28 23:53:13 -0600

    more review

--- js/practice.js
+++ js/practice.js
@@ -47,7 +47,55 @@ const myAnimals = [
         age: 1
     }
 ];
-for(let i = 0; i < myAnimals.length; i++){
- let animal = myAnimals[i];
- console.log('${animal.pet} are ${animal.age}');
+for (let i = 0; i < myAnimals.length; i++) {
+    let animal = myAnimals[i];
+    console.log('${animal.pet} are ${animal.age}');
+}
+
+const myStudents = [
+    {
+        firstName: 'Micah',
+        grade: 90
+    },
+    {
+        firstName: 'Coco',
+        grade: 99
+    },
+    {
+        firstName: 'Rob',
+        grade: 85
+    }
+];
+for (let i = 0; i < myStudents.length; i++) {
+    let student = myStudents[i];
+    console.log(`${student.firstName} scored ${student.grade}`);
+}
+//reverse loop
+const word = 'stressed';
+let reversedWord = ""
+for (let i = word.length - 1; i >= 0; i--) {
+    reversedWord += word[i];
+    console.log(reversedWord);
+}
+
+//Functions
+function grumpus(){
+    console.log('ugh! this isnt working!');
+    console.log('ugh! this isnt going to work!');
+    console.log('ugh! this isnt what i want to do right now!');
+}
+grumpus();
+for(let i =0; i < 50; i++){
+    grumpus();
+}
+
+//Dice roll function
+function rollDie(){
+    let roll = Math.floor(Math.random()*6) + 1;
+    console.log('Rolled: ${roll}');
+}
+
+//cards
+function getCard(){
+    2,3,4,5,6,7,8,9,10,J,Q,K,A
 }
