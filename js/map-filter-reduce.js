a5e24affa43a3539095d7303a33612213b4cfeec
Micah Larson <micahlarson@marks-mbp.lan>
2023-02-09 01:23:32 -0600

    fin

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -36,13 +36,41 @@ const users = [
     }
 ];
 //callback func will execute every user
-var filtered = users.filter(function(user) {
-   if(user.languages.length >= 3)
-    // console.log(user);
-    return true;
-})
+const filtered = users.filter(function (user) {
+    if (user.languages.length >= 3)
+        // console.log(user);
+        return true;
+});
 console.log(filtered);
 //filter has the ability to give back a new array
 //mod look at clock
-// progress report febuary 16th
+// progress report february 16th
 
+
+const emails = users.map(users => users.email);
+console.log(emails);
+
+//Use .reduce to get the total years of experience
+// from the list of users. Once you get the total of years
+// you can use the result to calculate the average.
+
+const totalYears = users.reduce((total, users) => {
+    return total + users.yearsOfExperience;
+},0);
+    const averageYearsOfEx = totalYears / users.length;
+    console.log(totalYears);
+    console.log(averageYearsOfEx);
+
+    const longestEmail = users.reduce((longest, user) => {
+        return user.email.length > longest.length ? user.email : longest;
+    }, '');
+//The ? is a shorthand way of writing a simple if-else statement in JavaScript
+//The [ ' ' ] is the initial value for the accumulator
+    console.log(longestEmail);
+
+    const names = users.reduce((sentence, user, index) => {
+        return index === users.length - 1 ?
+            sentence + user.name :
+            sentence + user.name + ',';
+    }, 'Your instructors are: ');
+    console.log(names);
