865becf10e20911d0c29fbc309da3526653453f3
Micah Larson <micahlarson@Marks-MBP.attlocal.net>
2023-02-08 20:12:16 -0600

    mapFilterReduce

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,48 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+//callback func will execute every user
+var filtered = users.filter(function(user) {
+   if(user.languages.length >= 3)
+    // console.log(user);
+    return true;
+})
+console.log(filtered);
+//filter has the ability to give back a new array
+//mod look at clock
+// progress report febuary 16th
+
